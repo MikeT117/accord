debb778621885089d68ca1730c13e3e0ba9bfc12
mike <cocoons_anvil_0n@icloud.com>
2024-01-07 05:57:58 +0000

    React Frontend: QueryClient now handles all query errors, mutation errors are still handled per request. Axios client updated to correctly update tokens if included in response.

--- frontend/apps/react-app/src/lib/axios.ts
+++ frontend/apps/react-app/src/lib/axios.ts
@@ -3,44 +3,44 @@ import { REST_API_ENDPOINT } from '@/constants';
 import { useSessionStore, sessionStore } from '@/shared-stores/sessionStore';
 
 export type AccordRESTAPIResponse = {
-  status: number;
-  successful: boolean;
+    status: number;
+    successful: boolean;
 };
 
 export type AccordRESTAPIUnsuccessfulResponse = {
-  detail?: string;
+    detail?: string;
 };
 
 export type AccordRESTAPISuccessfulResponse<T = any> = AccordRESTAPIResponse & {
-  data: T;
+    data: T;
 };
 
 const api = axios.create({
-  baseURL: REST_API_ENDPOINT,
+    baseURL: REST_API_ENDPOINT,
 });
 
 api.interceptors.response.use((response) => {
-  if (response.headers['Authorization']) {
-    sessionStore.setAccesstoken(response.headers['Authorization']);
-  }
-  return response;
+    if (response.headers['authorization']) {
+        sessionStore.setAccesstoken(response.headers['authorization']);
+    }
+    return response;
 });
 
 api.interceptors.request.use((config) => {
-  if (config.url?.includes('cloudinary')) {
+    if (config.url?.includes('cloudinary')) {
+        return config;
+    }
+
+    if (config.headers) {
+        config.headers['Authorization'] = useSessionStore.getState().accesstoken;
+        config.headers['X-App-Token'] = useSessionStore.getState().refreshtoken;
+    }
+
     return config;
-  }
-
-  if (config.headers) {
-    config.headers['Authorization'] = useSessionStore.getState().accesstoken;
-    config.headers['X-App-Token'] = useSessionStore.getState().refreshtoken;
-  }
-
-  return config;
 });
 
 api.defaults.validateStatus = (status) => {
-  return status < 400;
+    return status < 400;
 };
 
 export { api };
--- frontend/apps/react-app/src/lib/queryClient/queryClient.ts
+++ frontend/apps/react-app/src/lib/queryClient/queryClient.ts
@@ -1,3 +1,66 @@
-import { QueryClient } from '@tanstack/react-query';
+import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
+import { toastStore } from '../../shared-components/Toast';
+import { AxiosError } from 'axios';
+import { AccordRESTAPIResponse, AccordRESTAPIUnsuccessfulResponse } from '../axios';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry(failureCount, error) {
+                if (error instanceof AxiosError) {
+                    if (error.response?.status && error.response.status >= 500) {
+                        return failureCount < 5;
+                    }
+                }
+                return false;
+            },
+            retryDelay(failureCount) {
+                return failureCount * 2500;
+            },
+            retryOnMount: false,
+        },
+        mutations: {
+            retry: false,
+        },
+    },
+    mutationCache: new MutationCache({
+        onError: (error) => {
+            console.error(error);
+
+            let description = '';
+
+            if (error instanceof AxiosError) {
+                description =
+                    (error as AxiosError<AccordRESTAPIResponse & AccordRESTAPIUnsuccessfulResponse>)
+                        .response?.data.detail ?? '';
+            }
+
+            toastStore.create({
+                title: 'Request failed',
+                type: 'ERROR',
+                description,
+                duration: Infinity,
+            });
+        },
+    }),
+    queryCache: new QueryCache({
+        onError: (error) => {
+            console.error(error);
+
+            let description = '';
+
+            if (error instanceof AxiosError) {
+                description =
+                    (error as AxiosError<AccordRESTAPIResponse & AccordRESTAPIUnsuccessfulResponse>)
+                        .response?.data.detail ?? '';
+            }
+
+            toastStore.create({
+                title: 'Request failed',
+                type: 'ERROR',
+                description,
+                duration: Infinity,
+            });
+        },
+    }),
+});
