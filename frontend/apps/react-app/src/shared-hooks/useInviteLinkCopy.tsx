75a2797bc03121e4dc420ac30a95cc04f9de1d92
MikeT117 <cocoons_anvil_0n@icloud.com>
2024-02-22 01:59:51 +0000

    React Frontend: Adding translations, creating a toast on error in addition to success

--- frontend/apps/react-app/src/shared-hooks/useInviteLinkCopy.tsx
+++ frontend/apps/react-app/src/shared-hooks/useInviteLinkCopy.tsx
@@ -1,21 +1,27 @@
 import { useEffect } from 'react';
 import { toastStore } from '@/shared-components/Toast';
 import { useClipboard } from './useClipboard';
+import { useI18nContext } from '../i18n/i18n-react';
 
 export const useInviteLinkCopy = () => {
-  const { isError, isSuccess, onCopy } = useClipboard();
+    const { LL } = useI18nContext();
+    const { isError, isSuccess, onCopy } = useClipboard();
 
-  useEffect(() => {
-    if (isError) {
-      toastStore.addToast({ title: 'Failure copying link', type: 'ERROR', duration: 3000 });
-    }
-  }, [isError]);
+    useEffect(() => {
+        if (isSuccess) {
+            toastStore.create({
+                title: LL.Toasts.Titles.InviteLinkCopied(),
+                type: 'SUCCESS',
+                duration: 3000,
+            });
+        } else if (isError) {
+            toastStore.create({
+                title: LL.Toasts.Titles.InviteLinkCopyFailed(),
+                type: 'ERROR',
+                duration: 3000,
+            });
+        }
+    }, [isSuccess, isError]);
 
-  useEffect(() => {
-    if (isSuccess) {
-      toastStore.addToast({ title: 'Link copied to clipboard', type: 'SUCCESS', duration: 3000 });
-    }
-  }, [isSuccess]);
-
-  return onCopy;
+    return onCopy;
 };
