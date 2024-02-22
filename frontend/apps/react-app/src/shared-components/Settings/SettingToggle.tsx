077a1daefb7c7dde64ad9a98ea58813850fc8cfb
MikeT117 <cocoons_anvil_0n@icloud.com>
2024-02-22 01:43:16 +0000

    React Frontend: Allowing settings toggles to be disabled, I18n ysages in unsaved settings prompt

--- frontend/apps/react-app/src/shared-components/Settings/SettingToggle.tsx
+++ frontend/apps/react-app/src/shared-components/Settings/SettingToggle.tsx
@@ -2,22 +2,26 @@ import { forwardRef } from 'react';
 import { Switch } from '@/shared-components/Switch';
 
 export const SettingToggle = forwardRef<
-  HTMLDivElement,
-  { isChecked: boolean; onChange: () => void; label: string; helperText: string }
->(({ isChecked, onChange, label, helperText }, ref) => {
-  return (
+    HTMLDivElement,
+    {
+        onChange: () => void;
+        isChecked: boolean;
+        label: string;
+        helperText: string;
+        isDisabled?: boolean;
+    }
+>(({ onChange, isChecked, label, helperText, isDisabled = false }, ref) => (
     <div
-      className='flex w-full cursor-pointer select-none items-center justify-between rounded-md bg-grayA-3 p-3'
-      onClick={onChange}
-      ref={ref}
+        className='flex w-full cursor-pointer select-none items-center justify-between rounded-md bg-grayA-3 p-3'
+        onClick={onChange}
+        ref={ref}
     >
-      <div className='flex flex-col space-y-1'>
-        <label className='pointer-events-none text-sm font-medium text-gray-12'>{label}</label>
-        <span className='text-xs text-gray-11'>{helperText}</span>
-      </div>
-      <Switch checked={isChecked} />
+        <div className='flex flex-col space-y-1'>
+            <label className='pointer-events-none text-sm font-medium text-gray-12'>{label}</label>
+            <span className='text-xs text-gray-11'>{helperText}</span>
+        </div>
+        <Switch checked={isChecked} disabled={isDisabled} />
     </div>
-  );
-});
+));
 
 SettingToggle.displayName = 'SettingToggle';
--- frontend/apps/react-app/src/shared-components/Settings/UnsavedSettingsPrompt.tsx
+++ frontend/apps/react-app/src/shared-components/Settings/UnsavedSettingsPrompt.tsx
@@ -1,30 +1,38 @@
-import { InformationCircleIcon } from '@heroicons/react/24/solid';
 import { Button } from '@/shared-components/Button';
+import { Warning } from '@phosphor-icons/react';
+import { useI18nContext } from '../../i18n/i18n-react';
 
 export const UnsavedSettingsPrompt = ({
-  isVisible,
-  onDiscard,
-  onSave,
+    isModified,
+    isValid,
+    onDiscard,
+    onSave,
 }: {
-  isVisible: boolean;
-  onSave: () => void;
-  onDiscard: () => void;
+    isModified: boolean;
+    isValid?: boolean;
+    onSave: () => void;
+    onDiscard: () => void;
 }) => {
-  if (!isVisible) {
-    return null;
-  }
-  return (
-    <div className='flex w-full items-center rounded-md bg-grayA-3 p-3'>
-      <InformationCircleIcon className='h-8 w-8 text-yellowA-9' />
-      <span className='ml-3 mr-auto text-sm font-semibold text-gray-12'>
-        Careful - You have unsaved changes!
-      </span>
-      <div className='flex space-x-3'>
-        <Button intent='link' padding='s' onClick={onDiscard}>
-          Reset
-        </Button>
-        <Button onClick={onSave}>Save</Button>
-      </div>
-    </div>
-  );
+    const { LL } = useI18nContext();
+
+    if (!isModified) {
+        return null;
+    }
+
+    return (
+        <div className='flex w-full items-center rounded-md bg-grayA-3 p-3'>
+            <Warning size={32} weight='duotone' className='text-yellowA-9' />
+            <span className='ml-3 mr-auto text-sm font-semibold text-gray-12'>
+                {LL.Hints.UnsavedChanges()}
+            </span>
+            <div className='flex space-x-3'>
+                <Button intent='link' padding='s' onClick={onDiscard}>
+                    {LL.Actions.Reset()}
+                </Button>
+                <Button onClick={onSave} disabled={!isValid}>
+                    {LL.Actions.Save()}
+                </Button>
+            </div>
+        </div>
+    );
 };
