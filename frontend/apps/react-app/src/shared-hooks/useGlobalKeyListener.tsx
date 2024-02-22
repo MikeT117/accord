a2c056231fc21b39e9a6e652a83ad5a522dc1024
MikeT117 <cocoons_anvil_0n@icloud.com>
2024-02-22 01:58:49 +0000

    React Frontend: Formatting changes

--- frontend/apps/react-app/src/shared-hooks/useGlobalKeyListener.tsx
+++ frontend/apps/react-app/src/shared-hooks/useGlobalKeyListener.tsx
@@ -1,23 +1,31 @@
 import { useEffect } from 'react';
 
 export type GlobalKeyListenerParams = {
-  key: KeyboardEvent['key'];
-  isDisabled?: boolean;
-  onKeyPress: () => void;
+    key: KeyboardEvent['key'];
+    isDisabled?: boolean;
+    onKeyPress: () => void;
 };
 
 export const useGlobalKeyListener = ({
-  key,
-  isDisabled = false,
-  onKeyPress,
+    key,
+    isDisabled = false,
+    onKeyPress,
 }: GlobalKeyListenerParams) => {
-  useEffect(() => {
-    const listener = (e: KeyboardEvent) => {
-      if (e.key === key) onKeyPress();
-    };
-    if (!isDisabled) {
-      document.addEventListener('keyup', listener);
-    }
-    return () => document.removeEventListener('keyup', listener);
-  }, [isDisabled, onKeyPress, key]);
+    useEffect(() => {
+        if (isDisabled) {
+            return;
+        }
+
+        const listener = (e: KeyboardEvent) => {
+            if (e.key !== key) {
+                return;
+            }
+
+            onKeyPress();
+        };
+
+        document.addEventListener('keyup', listener);
+
+        return () => document.removeEventListener('keyup', listener);
+    }, [isDisabled, key, onKeyPress]);
 };
