cb01c6f2d7c42b71f566095d28fbf5bb00f66e6c
MikeT117 <cocoons_anvil_0n@icloud.com>
2024-02-23 13:35:05 +0000

    React Frontend: Adding translations, styling updates

--- frontend/apps/react-app/src/components/AppSidebar/AppSidebar.tsx
+++ frontend/apps/react-app/src/components/AppSidebar/AppSidebar.tsx
@@ -1,70 +1,91 @@
-import { HomeIcon, ServerStackIcon, Cog6ToothIcon } from '@heroicons/react/24/solid';
-import { PlusIcon } from '@heroicons/react/24/outline';
 import { useNavigate } from 'react-router-dom';
 import { IconButton } from '@/shared-components/IconButton';
-import { DefaultTooltip } from '@/shared-components/DefaultTooltip';
 import { guildCreatorStore } from '@/components/GuildCreator';
 import { useAppsidebarState } from './hooks/useAppsidebarState';
 import { currentUserSettingsStore } from '@/components/UserSettings';
 import { Avatar } from '@/shared-components/Avatar';
+import { AccordLogo } from '../../assets/AccordLogo';
+import { GearSix, Plus, Stack } from '@phosphor-icons/react';
+import { useI18nContext } from '../../i18n/i18n-react';
 
 export const AppSidebar = () => {
-  const appSidebarState = useAppsidebarState();
-  const navigate = useNavigate();
+    const { LL } = useI18nContext();
+    const appSidebarState = useAppsidebarState();
+    const navigate = useNavigate();
 
-  if (!appSidebarState.user) {
-    return null;
-  }
+    if (!appSidebarState) {
+        return null;
+    }
 
-  const { activeGuildId, guilds, user } = appSidebarState;
+    const { activeGuildId, guilds, user } = appSidebarState;
 
-  return (
-    <div className='col-span-1 row-span-full flex flex-col items-center overflow-y-auto border-r border-black p-3'>
-      <ul className='flex flex-col items-center space-y-3'>
-        <DefaultTooltip tootipText='User Dashboard' position='right' delayDuration={100}>
-          <IconButton padding='l' onClick={() => navigate('/app/@me')} intent='secondary'>
-            <HomeIcon className='h-5 w-5' />
-          </IconButton>
-        </DefaultTooltip>
-        <DefaultTooltip tootipText='Create Server' position='right' delayDuration={100}>
-          <IconButton padding='l' onClick={guildCreatorStore.toggleOpen} intent='secondary'>
-            <PlusIcon className='h-5 w-5 stroke-2' />
-          </IconButton>
-        </DefaultTooltip>
-        <DefaultTooltip tootipText='Public Servers' position='right' delayDuration={100}>
-          <IconButton
-            padding='l'
-            onClick={() => navigate('/app/public-servers')}
-            intent='secondary'
-          >
-            <ServerStackIcon className='h-5 w-5' />
-          </IconButton>
-        </DefaultTooltip>
-      </ul>
-      <ul className='my-6 flex flex-col items-center space-y-3'>
-        {guilds.map(({ id, name, icon }) => (
-          <DefaultTooltip key={id} tootipText={name} position='right' delayDuration={0}>
-            <div className='group relative flex items-center'>
-              {id === activeGuildId ? (
-                <div className='absolute left-[-10px] h-[70%] w-[3px] bg-grayA-8' />
-              ) : (
-                <div className='absolute left-[-10px] h-[0] w-[3px] bg-grayA-8 group-hover:h-[40%]' />
-              )}
-              <IconButton padding='xxs' onClick={() => navigate(`/app/server/${id}`)}>
-                <Avatar src={icon} fallback={name[0] + 'S'} size='xl' />
-              </IconButton>
+    return (
+        <div className='col-span-1 row-span-full flex mr-0.5 flex-col items-center overflow-y-auto bg-grayA-2 p-3'>
+            <ul className='flex flex-col items-center space-y-3'>
+                <IconButton
+                    padding='l'
+                    onClick={() => navigate('/app/@me')}
+                    intent='logo'
+                    tooltipText={LL.Tooltips.UserDashboard()}
+                    tooltipPosition='right'
+                    tooltipDelay={100}
+                >
+                    <AccordLogo className='h-5 w-5' />
+                </IconButton>
+
+                <IconButton
+                    padding='l'
+                    onClick={guildCreatorStore.open}
+                    intent='secondary'
+                    tooltipText={LL.Tooltips.CreateServer()}
+                    tooltipPosition='right'
+                    tooltipDelay={100}
+                >
+                    <Plus size={20} weight='bold' />
+                </IconButton>
+
+                <IconButton
+                    padding='l'
+                    onClick={() => navigate('/app/public-servers')}
+                    intent='secondary'
+                    tooltipText={LL.Tooltips.PublicServers()}
+                    tooltipPosition='right'
+                    tooltipDelay={100}
+                >
+                    <Stack size={20} weight='bold' />
+                </IconButton>
+            </ul>
+            <ul className='my-6 flex flex-col items-center space-y-3'>
+                {guilds.map(({ id, name, icon }) => (
+                    <div key={id} className='group relative flex items-center'>
+                        {id === activeGuildId ? (
+                            <div className='absolute left-[-10px] h-[70%] w-[3px] bg-grayA-10' />
+                        ) : (
+                            <div className='absolute left-[-10px] h-[0] w-[3px] bg-grayA-10 group-hover:h-[40%]' />
+                        )}
+                        <IconButton
+                            padding='xxs'
+                            onClick={() => navigate(`/app/server/${id}`)}
+                            tooltipText={name}
+                            tooltipPosition='left'
+                            tooltipDelay={0}
+                        >
+                            <Avatar src={icon} fallback={name[0] + 'S'} size='xl' />
+                        </IconButton>
+                    </div>
+                ))}
+            </ul>
+            <div className='mt-auto flex flex-col items-center'>
+                <IconButton
+                    onClick={currentUserSettingsStore.open}
+                    intent='secondary'
+                    tooltipText={LL.Tooltips.UserSettings()}
+                    tooltipPosition='right'
+                >
+                    <GearSix size={20} />
+                </IconButton>
+                <Avatar size='xl' className='mt-3' src={user.avatar} fallback={user.displayName} />
             </div>
-          </DefaultTooltip>
-        ))}
-      </ul>
-      <div className='mt-auto flex flex-col items-center'>
-        <DefaultTooltip tootipText='User Settings' position='right'>
-          <IconButton onClick={currentUserSettingsStore.toggleOpen} intent='secondary'>
-            <Cog6ToothIcon className='h-4 w-4' />
-          </IconButton>
-        </DefaultTooltip>
-        <Avatar size='xl' className='mt-3' src={user.avatar} fallback={user.displayName} />
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
--- frontend/apps/react-app/src/components/AppSidebar/hooks/useAppsidebarState.tsx
+++ frontend/apps/react-app/src/components/AppSidebar/hooks/useAppsidebarState.tsx
@@ -1,11 +1,15 @@
-import { useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { useGuildStore } from '@/shared-stores/guildStore';
 import { useCurrentUserStore } from '../../../shared-stores/currentUserStore';
 
 export const useAppsidebarState = () => {
-  const { guildId = '' } = useParams();
-  const guilds = useGuildStore(useCallback((s) => s.ids.map((i) => s.guilds[i]!), []));
-  const user = useCurrentUserStore(useCallback((s) => s.user, []));
-  return { activeGuildId: guildId, guilds, user };
+    const { guildId = '' } = useParams();
+    const guilds = useGuildStore((s) => s.ids.map((i) => s.guilds[i]!));
+    const user = useCurrentUserStore((s) => s.user);
+
+    if (!user) {
+        return null;
+    }
+
+    return { activeGuildId: guildId, guilds, user };
 };
