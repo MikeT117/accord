d2c255ad81f25d999be0744131f32744ba91ef29
MikeT117 <cocoons_anvil_0n@icloud.com>
2024-02-25 04:32:27 +0000

    React Frontend: formatting updates

--- frontend/apps/react-app/src/components/WaitForAuth/WaitForAuth.tsx
+++ frontend/apps/react-app/src/components/WaitForAuth/WaitForAuth.tsx
@@ -3,6 +3,6 @@ import { Navigate } from 'react-router-dom';
 import { useHasSession } from '@/shared-hooks';
 
 export const WaitForAuth = ({ children }: { children: ReactNode }) => {
-  const hasSession = useHasSession();
-  return hasSession ? <>{children}</> : <Navigate to='/' replace={true} />;
+    const hasSession = useHasSession();
+    return hasSession ? <>{children}</> : <Navigate to='/' replace={true} />;
 };
