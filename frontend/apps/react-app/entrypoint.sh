40e82052ef7fc4da7fe7b1ab3e713b6affa06a3d
MikeT117 <cocoons_anvil_0n@icloud.com>
2024-02-28 15:28:02 +0000

    React Frontend: Nginx conf moved to root of project, entrypoint updated to replace env variables in docker correctly

--- frontend/apps/react-app/entrypoint.sh
+++ frontend/apps/react-app/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-sed -i 's#__CLOUDINARY_URL__#'"$CLOUDINARY_URL"'#g' /usr/share/nginx/html/index.html
-sed -i 's#__CLOUDINARY_API_KEY__#'"$CLOUDINARY_API_KEY"'#g' index.html
-sed -i 's#__API_URL__#'"$API_URL"'#g' /usr/share/nginx/html/index.html
-sed -i 's#__RTC_URL__#'"$RTC_URL"'#g' /usr/share/nginx/html/index.html
-sed -i 's#__WS_URL__#'"$WS_URL"'#g' /usr/share/nginx/html/index.html
+sed -i 's#%VITE_HOST%#'"$HOST"'#g' /usr/share/nginx/html/index.html
+sed -i 's#%VITE_CLOUDINARY_URL%#'"$CLOUDINARY_URL"'#g' /usr/share/nginx/html/index.html
+sed -i 's#%VITE_CLOUDINARY_RES_URL%#'"$CLOUDINARY_RES_URL"'#g' index.html
+sed -i 's#%VITE_CLOUDINARY_API_KEY%#'"$CLOUDINARY_API_KEY"'#g' index.html
+sed -i 's#%VITE_API_URL%#'"$API_URL"'#g' /usr/share/nginx/html/index.html
+sed -i 's#%VITE_RTC_URL%#'"$RTC_URL"'#g' /usr/share/nginx/html/index.html
+sed -i 's#%VITE_WS_URL%#'"$WS_URL"'#g' /usr/share/nginx/html/index.html
 exec "$@"
