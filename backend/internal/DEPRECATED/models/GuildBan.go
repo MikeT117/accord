ab4768898f23501266fa06459f4fdda4cc240cfb
MikeT117 <cocoons_anvil_0n@icloud.com>
2025-07-01 01:51:10 +0100

    Removing deprecated code from branch

--- backend/internal/DEPRECATED/models/GuildBan.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/google/uuid"
-
-type GuildBan struct {
-	ID     uuid.UUID   `json:"id"`
-	Reason string      `json:"reason"`
-	User   UserLimited `json:"user"`
-}
