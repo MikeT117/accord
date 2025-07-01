b5d9962047caf0a94b71175d5d75e4e902e8f5e3
MikeT117 <cocoons_anvil_0n@icloud.com>
2025-07-01 01:48:42 +0100

    Renaming 'CreatePool' to 'MustCreatePool' to better represent how it "handles" errors

--- backend/internal/infra/db/repository_conn.go
+++ backend/internal/infra/db/repository_conn.go
@@ -7,16 +7,14 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func CreatePool(ctx context.Context, databaseURL string) *pgxpool.Pool {
+func MustCreatePool(ctx context.Context, databaseURL string) *pgxpool.Pool {
 
 	config, err := pgxpool.ParseConfig(databaseURL)
-
 	if err != nil {
 		log.Fatalf("Unable to parse config: %v\n", err)
 	}
 
 	pool, err := pgxpool.NewWithConfig(ctx, config)
-
 	if err != nil {
 		log.Fatalf("Unable to create Pool: %v\n", err)
 	}
