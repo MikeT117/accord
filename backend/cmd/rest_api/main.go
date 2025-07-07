30811c11658868190fa6e3271a4465da96a10623
MikeT117 <cocoons_anvil_0n@icloud.com>
2025-07-07 15:30:38 +0100

    Graceful shutdown and additional services added.

--- backend/cmd/rest_api/main.go
+++ backend/cmd/rest_api/main.go
@@ -2,19 +2,45 @@ package main
 
 import (
 	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/MikeT117/accord/backend/internal/application/services"
 	"github.com/MikeT117/accord/backend/internal/config"
 	"github.com/MikeT117/accord/backend/internal/infra/cloudinary"
 	"github.com/MikeT117/accord/backend/internal/infra/db"
 	"github.com/MikeT117/accord/backend/internal/infra/oauth"
+	"github.com/MikeT117/accord/backend/internal/infra/pubsub"
 	"github.com/MikeT117/accord/backend/internal/interface/api/rest"
 )
 
 func main() {
-	config := config.LoadConfig()
-	ctx := context.Background()
-	pool := db.CreatePool(ctx, config.DatabaseURL)
+	config := config.MustLoadConfig()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		cancel()
+	}()
+
+	// PGX Pool
+	pool := db.MustCreatePool(ctx, config.DatabaseURL)
+	defer func() {
+		log.Println("shutting down pgx pool")
+		pool.Close()
+	}()
+
+	// Event Broadcaster
+	eventPublisher := pubsub.MustCreateEventPublisher(ctx, config)
+	defer func() {
+		log.Println("shutting down eventPublisher")
+		eventPublisher.Close()
+	}()
 
 	// Transaction Handler
 	transactor, dbGetter := db.NewTransactorFromPool(pool)
@@ -41,23 +67,37 @@ func main() {
 	// Cloudinary
 	cloudinaryUpload := cloudinary.NewCloudinaryUpload(config.CloudinaryEnv, config.CloudinaryAPIKey, config.CloudinarySecret)
 
-	// Services
+	// Auth Services
 	authorisationService := services.CreateAuthorisationService(guildRoleRepository, channelRepository, guildMemberRepository, relationshipRepository)
 	authenticationService := services.CreateAuthenticationService(transactor, githubOAuth, userRepository, accountRepository)
+
+	// Event Service
+	eventService := services.CreateEventService(eventPublisher, channelMessageRepository, channelRepository, userRepository, guildRepository, guildMemberRepository, attachmentRepository, guildRoleRepository, relationshipRepository)
+
+	// Services
 	attachmentService := services.CreateAttachmentService(transactor, cloudinaryUpload, attachmentRepository)
 	accountService := services.CreateUserAccountService(transactor, accountRepository, userRepository)
-	channelMessageService := services.CreateChannelMessageService(transactor, authorisationService, channelMessageRepository, channelRepository, userRepository, guildMemberRepository, guildRoleRepository, attachmentRepository)
-	channelService := services.CreateChannelService(transactor, authorisationService, channelRepository, guildRoleRepository, userRepository)
+	channelService := services.CreateChannelService(transactor, authorisationService, eventService, channelRepository, guildRoleRepository, userRepository)
 	guildBanService := services.CreateGuildBanService(transactor, authorisationService, guildMemberRepository, guildBanRepository)
 	guildCategoryService := services.CreateGuildCategoryService(transactor, authorisationService, guildCategoryRepository)
 	guildInviteService := services.CreateGuildInviteService(transactor, authorisationService, guildInviteRepository, guildRepository)
 	guildMemberService := services.CreateGuildMemberService(transactor, authorisationService, userRepository, guildMemberRepository, guildRoleRepository, guildRepository, guildInviteRepository)
-	guildService := services.CreateGuildService(transactor, authorisationService, guildRepository, guildMemberRepository, guildRoleRepository, channelRepository)
-	guildRoleService := services.CreateGuildRoleService(transactor, authorisationService, guildRoleRepository, guildMemberRepository)
-	relationshipService := services.CreateRelationshipService(transactor, authorisationService, relationshipRepository, userRepository)
-	sessionService := services.CreateSessionService(transactor, sessionRepository, userRepository)
+	guildService := services.CreateGuildService(transactor, authorisationService, eventService, guildRepository, guildMemberRepository, guildRoleRepository, channelRepository)
+	guildRoleService := services.CreateGuildRoleService(transactor, authorisationService, eventService, guildRoleRepository, guildMemberRepository)
+	relationshipService := services.CreateRelationshipService(transactor, authorisationService, eventService, relationshipRepository, userRepository)
+	sessionService := services.CreateSessionService(transactor, eventService, sessionRepository, userRepository)
 	voiceStateService := services.CreateVoiceStateService(transactor, authorisationService, voiceStateRepository, userRepository)
+	channelMessageService := services.CreateChannelMessageService(transactor, authorisationService, eventService, channelMessageRepository, channelRepository, userRepository, guildMemberRepository, guildRoleRepository, attachmentRepository)
 
 	// Rest API
-	rest.CreateRouter(config, authenticationService, attachmentService, authorisationService, channelMessageService, channelService, guildBanService, guildCategoryService, guildInviteService, guildMemberService, guildRoleService, guildService, relationshipService, sessionService, accountService, voiceStateService)
+	server := rest.CreateRouter(config, authenticationService, attachmentService, authorisationService, channelMessageService, channelService, guildBanService, guildCategoryService, guildInviteService, guildMemberService, guildRoleService, guildService, relationshipService, sessionService, accountService, voiceStateService)
+	defer func() {
+		log.Println("shutting down echo router")
+		if err := server.Shutdown(ctx); err != nil {
+			server.Logger.Fatal(err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("shutting down rest api")
 }
