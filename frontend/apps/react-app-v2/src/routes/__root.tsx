6f8a5e28eb3a7ef05dd89d6f426827c862f002f4
MikeT117 <cocoons_anvil_0n@icloud.com>
2025-07-24 01:53:37 +0100

    Frontend: Wrapping app in queryclient provider, adding eventWebsocket to router context, initialising websocket connection inside a 'beforeLoad' .

--- frontend/apps/react-app-v2/src/routes/__root.tsx
+++ frontend/apps/react-app-v2/src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import type { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-import {} from "@/lib/valtio-stores/theme-store";
+import {} from "@/lib/valtio/stores/theme-store";
+import type { EventWebsocketType } from "@/lib/websocket/event-websocket";
 
 export const Route = createRootRouteWithContext<{
     queryClient: QueryClient;
+    eventWebsocket: EventWebsocketType;
 }>()({
     component: () => <Outlet />,
 });
--- frontend/apps/react-app-v2/src/routes/_auth/app/route.tsx
+++ frontend/apps/react-app-v2/src/routes/_auth/app/route.tsx
@@ -1,17 +1,25 @@
-import { tokensSchema } from "@/lib/zod-validation/token-schema";
-import { tokenStore } from "@/lib/valtio-stores/token-store";
+import { tokenStore } from "@/lib/valtio/stores/token-store";
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+import { AppSidebar } from "@/components/app-sidebar/app-sidebar";
+import { tokensSchema } from "@/lib/zod-validation/localstorage-schema";
+import { AppInitialisation } from "@/components/app-initialisation";
 
 export const Route = createFileRoute("/_auth/app")({
-    beforeLoad: () => {
+    beforeLoad: async ({ context, cause }) => {
         const { success } = tokensSchema.safeParse(tokenStore);
-        if (!success) {
-            throw redirect({ to: "/" });
-        }
+        if (!success) throw redirect({ to: "/" });
+        if (cause === "enter") await context.eventWebsocket.connect();
     },
+    onLeave: ({ context }) => context.eventWebsocket.shutdown(),
+    pendingComponent: () => <AppInitialisation />,
     component: RouteComponent,
 });
 
 function RouteComponent() {
-    return <Outlet />;
+    return (
+        <div className="grid h-screen w-screen grid-cols-[min-content_250px_1fr_min-content]">
+            <AppSidebar />
+            <Outlet />
+        </div>
+    );
 }
